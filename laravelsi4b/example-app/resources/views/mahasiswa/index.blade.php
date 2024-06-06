903a28e40434dc8da9776a0fc08c96817bc1d59d
rqz234 <mrifqanzalbina_2226240103@mhs.mdp.ac.id>
2024-06-06 15:16:50 +0700

    Update

--- laravelsi4b/example-app/resources/views/mahasiswa/index.blade.php
+++ laravelsi4b/example-app/resources/views/mahasiswa/index.blade.php
@@ -34,12 +34,11 @@
                             <tr>
                                 <td> {{ $item["npm"] }}</td>
                                 <td> {{ $item["nama"] }}</td>
-                                <td><img src="{{ url('foto/'.$item["url_foto"]) }}"></td>
                                 <td> {{ $item["tempat_lahir"] }}</td>
                                 <td> {{ $item["tanggal_lahir"] }}</td>
                                 <td> {{ $item["alamat"] }}</td>
                                 <td> {{ $item["prodi"]["nama"] }}</td>
-                                <td> {{ $item["url_foto"] }}</td>
+                                <td><img src="{{ url('foto/'.$item["url_foto"]) }}"></td>
                                 <td>
                                     <form action="{{ route('mahasiswa.destroy', $item['id']) }}" method="post">
                                         @method('DELETE')
@@ -62,7 +61,7 @@
 @if (session('success'))
   <script>
     Swal.fire({
-    title: "Sukses",
+    title: "",
     text: "{{ session('success') }}",
     icon: "success"
   });
