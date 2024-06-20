92b34c2ab1530d35beb90a091673b6f3356c56ed
rifqanzalbina <mrifqanzalbina_2226240103@mhs.mdp.ac.id>
2024-06-20 18:22:04 +0700

    Update

--- config/database.php
+++ config/database.php
@@ -61,12 +61,12 @@ return [
 
         'mariadb' => [
             'driver' => 'mariadb',
-            'url' => env('MYSQL_ADDON_URI'),
-            'host' => env('MYSQL_ADDON_HOST', '127.0.0.1'),
-            'port' => env('MYSQL_ADDON_PORT', '3306'),
-            'database' => env('MYSQL_ADDON_DB', 'laravel'),
-            'username' => env('MYSQL_ADDON_USER', 'root'),
-            'password' => env('MYSQL_ADDON_PASSWORD', ''),
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
