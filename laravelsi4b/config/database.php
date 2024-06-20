fc96c3625a1f2c848bd644e4aa1e5564a4dfb9f4
rqz234 <mrifqanzalbina_2226240103@mhs.mdp.ac.id>
2024-06-20 14:13:45 +0700

    Update

--- laravelsi4b/config/database.php
+++ laravelsi4b/config/database.php
@@ -41,12 +41,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => env('MYSQL_ADDON_HOST', '127.0.0.1'),
+            'port' => env('MYSQL_ADDON_PORT', '3306'),
+            'database' => env('MYSQL_ADDON_DB', 'laravel'),
+            'username' => env('MYSQL_ADDON_USER', 'root'),
+            'password' => env('MYSQL_ADDON_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
